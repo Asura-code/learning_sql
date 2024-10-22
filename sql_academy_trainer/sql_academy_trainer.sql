01ddb587f239a89adb88229f5a88666362d7cc1a
Asura <bankaikinowork@gmail.com>
2024-10-22 21:30:56 +0300

    Day 2
    11-15

--- sql_academy_trainer/sql_academy_trainer.sql
+++ sql_academy_trainer/sql_academy_trainer.sql
@@ -16,6 +16,11 @@ WHERE town_from = 'Moscow'
 SELECT name FROM passenger
 WHERE name LIKE '%man'
 
+-- через регулярные выражения
+
+SELECT name FROM passenger
+WHERE name REGEXP  'man$'
+
 --5. Вывести количество рейсов, совершенных на TU-134
 
 SELECT COUNT(*) as count FROM trip
@@ -49,3 +54,36 @@ WHERE  town_from = 'Vladivostok'
 SELECT * FROM Trip 
 WHERE time_out BETWEEN 
 '1900-01-01 10:00:00' AND '1900-01-01 14:00:00'
+
+
+--11. Выведите пассажиров с самым длинным ФИО. Пробелы, дефисы и точки считаются частью имени.
+
+SELECT name FROM Passenger 
+WHERE LENGTH(name) = (SELECT MAX(LENGTH(name)) FROM Passenger)
+
+--12. Выведите идентификаторы всех рейсов и количество пассажиров на них. Обратите внимание, что на каких-то рейсах пассажиров может не быть. В этом случае выведите число "0".
+
+SELECT Trip.id , COUNT(Pass_in_trip.passenger) AS count 
+FROM  Trip 
+LEFT JOIN Pass_in_trip ON Pass_in_trip.trip = Trip.id
+GROUP BY Trip.id
+
+--13. Вывести имена людей, у которых есть полный тёзка среди пассажиров
+
+SELECT name FROM Passenger
+GROUP BY name
+HAVING COUNT(name) > 1
+
+--14. В какие города летал Bruce Willis
+
+SELECT town_to FROM Trip
+JOIN Pass_in_trip ON Trip.id = Pass_in_trip.trip
+JOIN Passenger ON Passenger.id = Pass_in_trip.passenger
+WHERE Passenger.name = 'Bruce Willis'
+
+--15. Выведите дату и время прилёта пассажира Стив Мартин (Steve Martin) в Лондон (London)
+
+SELECT time_in FROM Trip
+JOIN Pass_in_trip ON Trip.id = Pass_in_trip.trip
+JOIN Passenger ON Passenger.id = Pass_in_trip.passenger
+WHERE Passenger.name = 'Steve Martin' and Trip.town_to = 'London'
